e8d1b98e2e4a3749d38dc6eddf9a545ed5b40385
authZhang <15810801127@163.com>
2020-09-25 21:58:46 +0800

    通过AtomicInteger原子类当做自旋锁，两个线程交替打印数组

--- auth-common/src/main/java/com/auth/thread/PrintArraysThread.java
+++ auth-common/src/main/java/com/auth/thread/PrintArraysThread.java
@@ -100,44 +100,42 @@ public class PrintArraysThread {
     }
 
     private static final AtomicInteger flag = new AtomicInteger(1);
-
     /**
      * 通过原子类AtomicInteger自旋判断本线程是否应该执行
+     * 当自旋值不是自己的值时，循环等待
+     * 当自旋值是自己的值时，执行打印，同时将自旋值设置为另一个线程的自旋值，让另一个线程得以执行
      */
     private static void method3() {
         int[] params = {2, 4, 6, 8, 0};
         Thread t1 = new Thread(() -> {
-            for (int i = 0; i < params.length; i++) {
-                while (flag.get() == 1) {
-                    //奇数
-                    if ((i & 1) == 0) {
-                        System.out.println(Thread.currentThread().getName() + ":" + params[i]);
-                        flag.set(2);
-                    }
+            for (int i = 0; i<params.length;i++){
+                while (flag.get() != 1) {
                 }
-
+                //奇数
+                if ((i & 1) == 0) {
+                    System.out.println(Thread.currentThread().getName() + ":" + params[i]);
+                    flag.set(2);
+                }
             }
         }, "奇数位线程");
-
         Thread t2 = new Thread(() -> {
-            for (int i = 0; i < params.length; i++) {
-                while (flag.get() == 2) {
-                    //偶数
-                    if ((i & 1) == 1) {
-                        System.out.println(Thread.currentThread().getName() + ":" + params[i]);
-                        flag.set(1);
-                    }
+            for (int i = 0; i<params.length;i++){
+                while (flag.get() != 2) {
                 }
-
+                //偶数
+                if ((i & 1) == 1) {
+                    System.out.println(Thread.currentThread().getName() + ":" + params[i]);
+                    flag.set(1);
+                }
             }
-        }, "偶数位线程");
 
+        }, "偶数位线程");
         t1.start();
         t2.start();
     }
 
     public static void main(String[] args) {
-        method1();
+        method3();
     }
 
 }
