18d3db1c67bd6597d8cffce8e3ede6f28c2ef1f6
authZhang <15810801127@163.com>
2020-10-02 17:58:21 +0800

    通过信号量实现，两个线程交替打印数组

--- auth-common/src/main/java/com/auth/thread/PrintArraysThread.java
+++ auth-common/src/main/java/com/auth/thread/PrintArraysThread.java
@@ -1,5 +1,6 @@
 package com.auth.thread;
 
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.LockSupport;
 
@@ -51,9 +52,6 @@ public class PrintArraysThread {
         }, "偶数位线程");
         t1.start();
         t2.start();
-    }
-    public static void main(String[] args) {
-        method1();
     }
 
     private static Thread t2 = new Thread();
@@ -123,5 +121,45 @@ public class PrintArraysThread {
         }, "偶数位线程");
         t1.start();
         t2.start();
+    }
+
+    private static final Semaphore semaphore = new Semaphore(1);
+    /**
+     * 通过信号量实现
+     */
+    private static void method4() {
+        int[] params = {2, 4, 6, 8, 0};
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < params.length; i++) {
+                try {
+                    semaphore.acquire();
+                }catch (InterruptedException e){
+                }
+                //奇数
+                if ((i & 1) == 0) {
+                    System.out.println(Thread.currentThread().getName() + ":" + params[i]);
+                }
+                semaphore.release();
+            }
+        }, "奇数位线程");
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < params.length; i++) {
+                try {
+                    semaphore.acquire();
+                }catch (InterruptedException e){
+                }
+                //偶数
+                if ((i & 1) == 1) {
+                    System.out.println(Thread.currentThread().getName() + ":" + params[i]);
+                }
+                semaphore.release();
+            }
+        }, "偶数位线程");
+        t1.start();
+        t2.start();
+    }
+
+    public static void main(String[] args) {
+        method4();
     }
 }
