29c6a9a0cb0c86b14ef56a718f91b6b373a444c0
zhangyu <zhangyu122@tal.com>
2021-02-02 17:31:43 +0800

    commit

--- auth-arithmetic/src/main/java/com/zhangyu/leetcode/problem01_50/Problems20.java
+++ auth-arithmetic/src/main/java/com/zhangyu/leetcode/problem01_50/Problems20.java
@@ -41,9 +41,7 @@ package com.zhangyu.leetcode.problem01_50;
 //输出: true
 // Related Topics 栈 字符串
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * @description:
@@ -57,7 +55,7 @@ public class Problems20 {
     }
 
     /**
-     * 解答成功:
+     *              解答成功:
      * 				执行耗时:5 ms,击败了9.19% 的Java用户
      * 				内存消耗:38.5 MB,击败了7.65% 的Java用户
      * @description:
@@ -90,31 +88,37 @@ public class Problems20 {
         return stack.isEmpty();
     }
 
-    // TODO: 2021/1/6 待优化 
+    /**
+     * @description:
+     *
+     *              解答成功:
+     * 				执行耗时:3 ms,击败了25.36% 的Java用户
+     * 				内存消耗:36.6 MB,击败了57.05% 的Java用户
+     *
+     * @author: zhangyu122
+     * @date: 2021/2/2 5:31 下午
+     */
     private static boolean check1(String s){
-        if (s == null) return false;
-        if (s.length() == 0) return true;
-        List<String> baseChar = new ArrayList<>();
-        baseChar.add("()");
-        baseChar.add("{}");
-        baseChar.add("[]");
-        char[] chars = s.toCharArray();
-        if (chars.length % 2 != 0) return false;
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0 ; i < chars.length ; i++){
-            char currentChar = chars[i];
-            if (stack.isEmpty()){
-                stack.push(currentChar);
-                continue;
-            }
-            Character preItem = stack.pop();
-            String currentString = preItem.toString()+ currentChar;
-            if (!currentString.equals("()") && !currentString.equals("{}") && !currentString.equals("[]")){
-                stack.push(preItem);
-                stack.push(currentChar);
-            }
-            if ((i > (chars.length / 2 +1)) && !stack.isEmpty()){
-                return false;
+        if (s == null || s.length() % 2 != 0) {
+            return false;
+        }
+        Map<Character,Character> map =new HashMap<>();
+        map.put('(',')');
+        map.put('{','}');
+        map.put('[',']');
+        Deque<Character> stack = new ArrayDeque<>();
+        for (int i = 0 ;i < s.length();i++){
+            char c = s.charAt(i);
+            if (map.containsKey(c)){
+                stack.push(c);
+            }else {
+                if (stack.isEmpty()) return false;
+                Character peek = stack.peek();
+                Character character = map.get(peek);
+                if (!character.equals(c)){
+                    return false;
+                }
+                stack.poll();
             }
         }
         return stack.isEmpty();
